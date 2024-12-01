35197e87881124828baf71f779f9394403df045e
Aleksandr <gggols32@gmail.com>
2024-12-01 11:40:41 +0300

    Add routes and views for detail page, vote page, results page / Add templates for index page and detail page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 
-def index(requets: HttpRequest) -> HttpResponse:
-  return HttpResponse("Hello, Otus!")
+from .models import Question
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "polls/detail.html", {"question": question})
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+  return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+  return HttpResponse(f"You're voting on question {question_id}")
+
+def index(request: HttpRequest) -> HttpResponse:
+  latest_question_list = Question.objects.order_by("-pub_date")[:5]
+  context = {
+    "latest_question_list": latest_question_list
+  }
+  return render(request, "polls/index.html", context)
