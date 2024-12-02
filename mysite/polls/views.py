68294829376c194576f42ba5d83830d25359831d
Aleksandr <gggols32@gmail.com>
2024-12-02 15:23:33 +0300

    Add tests for Question model, Imdex view and Detail view / Small fixes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -37,11 +38,21 @@ class IndexView(generic.ListView):
   context_object_name = "latest_question_list"
 
   def get_queryset(self):
-    return Question.objects.order_by("-pub_date")[:5]
+    """
+    Return the last five published questions (not including
+    those set to be published in the future)
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
   model = Question
   template_name = "polls/detail.html"
+
+  def get_queryset(self):
+    """
+    Exclude any questions that aren't published yet.
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
   model = Question
